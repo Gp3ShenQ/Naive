d87f4421c5e6485488bc14f1ef84c24a90a00eaa
Amos <gp3g4xup6@gmail.com>
2024-11-13 18:31:28 +0800

    [新增] tailwind && 三大區塊組件

--- src/layout/LayoutView.vue
+++ src/layout/LayoutView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div></div>
+  <TopHeaderView />
+  <LayoutView />
+  <FooterView />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import FooterView from '../components/FooterView.vue';
+import LeftMenuView from '../components/LeftMenuView.vue';
+import TopHeaderView from '../components/TopHeaderView.vue';
+</script>
 
 <style scoped lang="scss"></style>
--- src/router/router.js
+++ src/router/router.js
@@ -5,17 +5,6 @@ export const routes = [
     path: '/',
     component: () => import('../layout/LayoutView.vue'),
   },
-  // {
-  // path: '/',
-  // component: () => import('../layout/LayoutView.vue'),
-  // children: [
-  //   // 在這裡添加子路由
-  //   {
-  //     path: '',
-  //     component: () => import('../layout/')
-  //   }
-  // ]
-  // }
 ];
 
 const router = createRouter({
